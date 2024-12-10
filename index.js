9637b45d2e58ffda8a4ec1fa9607cc1eeb75ce52
Adrian Garza <adriangarza236@yahoo.com>
2024-12-10 08:16:09 -0600

    completed function lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickUpLocation) {
+    if (pickUpLocation >= 42) {
+        const distanceInBlocks = (pickUpLocation - 42)
+        return distanceInBlocks
+    } 
+    else if (pickUpLocation < 42) {
+        const distanceInBlocks = (42 - pickUpLocation)
+        return distanceInBlocks
+    }
+};
+function distanceFromHqInFeet(pickUpLocation) {
+    const distanceInFeet = (distanceFromHqInBlocks(pickUpLocation) * 264)
+    return distanceInFeet
+};
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination) {
+        const numberOfFeetTravelled = ((start - destination) * 264)
+        return numberOfFeetTravelled
+    }
+    else if (start < destination) {
+        const numberOfFeetTravelled = ((destination - start) * 264)
+        return numberOfFeetTravelled 
+    }
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        return ((distanceTravelledInFeet(start, destination) - 400) * .02)
+    }
+    else if (distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far"
+    }
+
+}
